c2aa7e16defbab2aedfd71f2c5045161d75ebf33
Gabriel <gabrielulu@gmail.com>
2018-01-15 13:42:01 +0800

    adjusting post request to take in upvote/downvote req.body

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var port = process.env.PORT || 8080;
 var mlab = process.env.MONGODB_URI || 'mongodb://localhost/questionbank'
 // const dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost/project2'
 
-
 mongoose.connect(mlab, {
   useMongoClient: true
 })
@@ -24,7 +23,6 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 var questSearch = (res) => {
-  // res.render('home')
   Question.find({}, (err, quest) => {
     if (err) return console.log(err)
     res.render('home', {name: 'fellow WDI 12 Hustler.', qList: quest})
@@ -35,16 +33,29 @@ var questSearch = (res) => {
 
 app.get('/', (req, res) => {
   questSearch(res)
-  // res.render('home')
 })
 
 app.post('/', (req, res) => {
-  // console.log('testing 1 trigger', req.body.question)
-  Question.create({question: req.body.question, score: 0}, function (err, question) {
-    if (err) return console.log(err)
-    console.log('question added', req.body.question)
-  })
+  console.log('triggered here for', Object.keys(req.body)[0])
+  if (req.body.question) {
+    Question.create({question: req.body.question, score: 0}, function (err, question) {
+      if (err) return console.log(err)
+      console.log('question added', req.body.question)
+    })
+  } else {
+    Question.findByIdAndUpdate(Object.keys(req.body)[0], { $set: { score: 1 }})
+  }
   res.redirect('/')
+  console.log('redirected')
+})
+
+app.post('/vote', (req, res) => {
+  console.log(this.value) // TODO: upon clicking button, find the question entry, and add/subtract score
+  // Question.create({question: req.body.question, score: 0}, function (err, question) {
+  //   if (err) return console.log(err)
+  //   console.log('question added', req.body.question)
+  // })
+  // res.redirect('/')
 })
 app.listen(port, () => {
   console.log('App is running on port 8080')
